3c5e299e43249d683be8f8c40a335ebe488a4208
Ebubexx <93883757+Ebubexx@users.noreply.github.com>
2022-02-13 12:33:45 -0500

    Update Script.js
    Accessed NASA's APOD API with a jQuery get command. Created a function to print the data as a spacetagram post.

--- Script.js
+++ Script.js
@@ -12,3 +12,21 @@ $(document).ready(function () {
     }
   });
 });
+
+$.get("https://api.nasa.gov/planetary/apod?api_key=ghDRbGoN2ROByGuggVmgmF0nN4XSuSgJrgdoD9hF&count=2&thumbs=True", function (data) {
+  console.log(typeof data); // string
+  console.log(data); // HTML content of the jQuery.ajax page
+  var firstPost = data[0]
+  makePost(firstPost["url"], firstPost["date"], firstPost["title"], firstPost["explanation"]);
+});
+
+function makePost(imgurl, date, title, description) {
+  var post = `
+    <section class="section1">
+      <img class="img1" src="${imgurl}" alt="${title}">
+      <span class="name">${title}</span> <span class="date">${date}</span>
+      <p class="p1">${description}</p>
+      <button class="likebutton">Like</button>
+    </section> `;
+  $("main").append(post);
+} 
